f383d5a4ebfae9f7ba41fcfe2ad6bdd7e82dfdc4
Anis Subba <21302329@student.uwl.ac.uk>
2026-01-19 00:01:56 +0000

    End to end journey is done and tested
     - Might require more assertion in future
    
     - New Class added to check various types of locator playwright has to offer

--- tests/EcommerFullJourney.spec.js
+++ tests/EcommerFullJourney.spec.js
@@ -13,6 +13,8 @@ test.only('rahulshettyacademy automation: Smoke Test', async ({browser}) =>
     const productsTitle = 'ZARA COAT 3';
     const cartButton = page.locator("[routerlink*='cart']");
     const email = "test2014@gmail.com";
+    const TextBoxes = page.locator(".input.txt");
+    const dateBox = page.locator(".ddl");
 
     // Navigate to the application
     await page.goto('https://rahulshettyacademy.com/client');
@@ -75,11 +77,14 @@ test.only('rahulshettyacademy automation: Smoke Test', async ({browser}) =>
 
     expect(await page.locator(".user__name label").textContent()).toContain(email);
 
-    //card details
-    /* await page.locator("[placeholder='Card Number']").fill("4111 1111 1111 1111");
-    await page.locator("[placeholder='MM / YY']").fill("12/25");
-    await page.locator("[placeholder='CVC']").fill("123");
-    await page.locator("[placeholder='Name on Card']").fill("Test User"); */
+    //card details entry
+    await TextBoxes.nth(0).fill("4111111111111111");
+    await TextBoxes.nth(1).fill("123");
+    await TextBoxes.nth(2).fill("Tester Testerson");
+    
+    //Expiry date selection
+    await dateBox.nth(0).selectOption("12");
+    await dateBox.nth(1).selectOption("30");
 
     // Place the order
     await page.locator(".action__submit").click();
@@ -89,6 +94,32 @@ test.only('rahulshettyacademy automation: Smoke Test', async ({browser}) =>
     const arraytext = orderId.split("|");
     const finalOrderId = arraytext[1].split("||")[0].trim(); 
     console.log("Order ID: " + finalOrderId);
+
+    // Go to Orders details page. can you button[routerlink*='myorders'] or li [routerlink*='myorders']
+    await page.locator("li [routerlink*='myorders']").click();
+
+    // wait for orders page to load
+    await page.locator("tbody").waitFor();
+
+    // Orders Page - Verify order in orders list: can use tbody tr or [scope*='row']
+    const orderIDGroup = await page.locator("[scope*='row']");
+    const orderCount = await orderIDGroup.count();
+    for (let i = 0; i < orderCount; ++i)
+    {   
+        const orderIDText = await orderIDGroup.nth(i).locator("th").textContent();
+        if (orderIDText === finalOrderId)
+        {   
+            await orderIDGroup.nth(i).locator("button:has-text('View')").click();
+            break;
+        }
+    }
+
+    // Order Details Page - Verify order ID
+    const orderDetailsId = await page.locator(".-main").textContent();
+    console.log("Confirmed Order Details ID matches the order ID during order made: " + orderDetailsId);
+    expect(finalOrderId.includes(orderDetailsId)).toBeTruthy();
+
+
 
     await page.pause();
 });
